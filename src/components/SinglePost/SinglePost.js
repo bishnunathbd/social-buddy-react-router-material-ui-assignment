6429f1b7ab60b8cd62eda2f12c187522e75a5948
bishnunathbd <bishnunath774@gmail.com>
2020-12-02 09:06:56 +0600

    added like btn

--- src/components/SinglePost/SinglePost.js
+++ src/components/SinglePost/SinglePost.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '@material-ui/core/Button';
 import { Link } from 'react-router-dom';
+import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
 
 const SinglePost = (props) => {
   // console.log(props);
   const { title, body, id } = props.post;
+
+  const [likeColor, setLikeColor] = useState('');
+
+  const handleLike = () => {
+    const color = likeColor ? '' : 'primary';
+    setLikeColor(color);
+  }
 
   const singlePostStyle = {
     width: '80%',
@@ -19,6 +27,7 @@ const SinglePost = (props) => {
     <div style={singlePostStyle}>
       <h3>{title}</h3>
       <p>{body}</p>
+      <ThumbUpAltIcon onClick={handleLike} color={likeColor}></ThumbUpAltIcon>
       <Link to={`/postID/${id}`}>
         <Button variant="contained" color="primary">See More</Button>
       </Link>
